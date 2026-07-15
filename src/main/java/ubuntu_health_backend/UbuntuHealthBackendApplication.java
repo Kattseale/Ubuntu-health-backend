24eabb5c9914e06b2a089dc176eacc3f622c8809
Katt <kthutsana7@gmail.com>
2026-07-15 20:28:47 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/ubuntu_health_backend/UbuntuHealthBackendApplication.java
@@ -0,0 +1,13 @@
+package ubuntu_health_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbuntuHealthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbuntuHealthBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ubuntu_health_backend/UbuntuHealthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ubuntu_health_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbuntuHealthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
