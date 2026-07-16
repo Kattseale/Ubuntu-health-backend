c1579bd253368966434eec354b39d132f648fd6d
ZiziphoN <ziziphonjenjese920@gmail.com>
2026-07-16 15:12:37 +0200

    Space

--- src/main/java/com/ubuntuhealth/UbuntuHealthApplication.java
+++ src/main/java/com/ubuntuhealth/UbuntuHealthApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class UbuntuHealthApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UbuntuHealthApplication.class, args);
 	}
