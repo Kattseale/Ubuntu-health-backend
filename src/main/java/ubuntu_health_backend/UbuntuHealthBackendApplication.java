b1b3843dc714f5432519f4902d88e844ba6c17d0
System Developer <222059958@edu.vut.ac.za>
2026-07-18 15:58:58 +0200

    Update UbuntuHealthBackendApplication.java

--- src/main/java/ubuntu_health_backend/UbuntuHealthBackendApplication.java
+++ src/main/java/ubuntu_health_backend/UbuntuHealthBackendApplication.java
@@ -1,13 +1,14 @@
-package ubuntu_health_backend;
+package com.ubuntuhealth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ubuntuhealth")
 public class UbuntuHealthBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UbuntuHealthBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UbuntuHealthBackendApplication.class, args);
+    }
 }
